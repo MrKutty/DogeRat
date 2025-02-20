1a1c3b6c9dab7b6609f465dd2c2e2cd8bea4b907
MrKutty <111560242+MrKutty@users.noreply.github.com>
2025-02-20 17:47:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = process.7902268014:AAHeusarKlEnGY0y65PXhmhmHN6c2k4JjAc
+const token = 7902268014:AAHeusarKlEnGY0y65PXhmhmHN6c2k4JjAc
 const id = 5493981710
 const address = 'https://www.google.com'
 
